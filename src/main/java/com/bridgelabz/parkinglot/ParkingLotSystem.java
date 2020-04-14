8be22f3558636f8525a4c342d8e4d22f5bcdc185
Devki1 <gdev3195@gmail.com>
2020-04-14 19:24:06 +0530

    ADDED:Display a welcome message of parkinglot

---
+++ src/main/java/com/bridgelabz/parkinglot/ParkingLotSystem.java
@@ -0,0 +1,7 @@
+package com.bridgelabz.parkinglot;
+
+public class ParkingLotSystem {
+    public static void main(String[] args) {
+        System.out.println("Welcome in parkinglot ");
+    }
+    }
