712ea43e7abba596decef34f15671c5510724453
Devki1 <gdev3195@gmail.com>
2020-04-16 14:55:34 +0530

    REFACTORED:Throw exception in park method

--- src/main/java/com/bridgelabz/parkinglot/ParkingLotSystem.java
+++ src/main/java/com/bridgelabz/parkinglot/ParkingLotSystem.java
@@ -3,9 +3,11 @@ package com.bridgelabz.parkinglot;
 public class ParkingLotSystem {
     private Object vehicle;
 
-    public boolean park(Object vehicle) {
+    public boolean park(Object vehicle) throws ParkingLotException {
+        if (this.vehicle != null)
+            throw new ParkingLotException(ParkingLotException.ExceptionType.PARKING_LOT_IS_FULL,"Parking lot is full");
         this.vehicle = vehicle;
-        return true;
+         return true;
     }
 
     public boolean unPark(Object vehicle) {
