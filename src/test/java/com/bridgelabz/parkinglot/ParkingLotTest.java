cb885572154ee9d53b6647370f69ad05a61f9abe
Devki1 <gdev3195@gmail.com>
2020-04-19 10:59:56 +0530

    ADDED:Test cases according to Observer pattern

--- src/test/java/com/bridgelabz/parkinglot/ParkingLotTest.java
+++ src/test/java/com/bridgelabz/parkinglot/ParkingLotTest.java
@@ -1,18 +1,26 @@
 package com.bridgelabz.parkinglot;
 
-import com.bridgelabz.parkinglot.service.ParkingLotException;
+import com.bridgelabz.parkinglot.Observer.AirportSecurity;
+import com.bridgelabz.parkinglot.Observer.ParkingLotException;
+import com.bridgelabz.parkinglot.Observer.ParkingLotOwner;
+import com.bridgelabz.parkinglot.service.ParkingLotSystem;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
 public class ParkingLotTest {
+
     ParkingLotSystem parkingLotSystem = null;
     Object vehicle = null;
+    ParkingLotOwner owner = null;
+    AirportSecurity airportSecurity = null;
 
     @Before
-    public void setUp() {
+    public void setUp() throws Exception {
         parkingLotSystem = new ParkingLotSystem(5);
         vehicle = new Object();
+        owner = new ParkingLotOwner();
+       airportSecurity = new AirportSecurity();
     }
 
     @Test
@@ -23,7 +31,7 @@ public class ParkingLotTest {
     }
 
     @Test
-    public void givenAVehicle_WhenUnParked_ShouldReturnTrue() throws ParkingLotException {
+    public void givenAVehicle_WhenUnParked_ShouldReturnTrue() throws  ParkingLotException {
         parkingLotSystem.park("Toyota Fortuner");
         parkingLotSystem.unPark("Toyota Fortuner");
         boolean isVehicleUnParked = parkingLotSystem.isVehicleParked();
@@ -56,7 +64,7 @@ public class ParkingLotTest {
     }
 
     @Test
-    public void givenAVehicle_WhenAlreadyUnParkedAndTriedToUnParkAgain_ShouldThrowException() throws ParkingLotException {
+    public void givenAVehicle_WhenAlreadyUnParkedAndTriedToUnParkAgain_ShouldThrowException() {
         try {
             parkingLotSystem.park("Tata Indigo CS");
             parkingLotSystem.unPark("Tata Indigo CS");
@@ -64,5 +72,46 @@ public class ParkingLotTest {
         } catch (ParkingLotException e) {
             Assert.assertEquals(ParkingLotException.ExceptionType.VEHICLE_ALREADY_UNPARKED_OR_WRONG_VEHICLE, e.type);
         }
+    }
+
+    @Test
+    public void givenParkingLotIsFull_OwnerShouldShowFullSign() throws ParkingLotException {
+        parkingLotSystem.register(owner);
+        parkingLotSystem.park("Tata Indigo CS");
+        parkingLotSystem.park("Toyota Fortuner");
+        parkingLotSystem.park("Maruti Swift Dzire");
+        parkingLotSystem.park("Tata Hexa");
+        parkingLotSystem.park("Maruti 800");
+        Assert.assertEquals(owner.getFlag(), ParkingLotOwner.Flag.PARKING_IS_FULL);
+    }
+
+    @Test
+    public void givenParkingLotIsNotFull_OwnerShouldShowVacantSign() throws ParkingLotException {
+        parkingLotSystem.register(owner);
+        parkingLotSystem.park("Tata Indigo CS");
+        parkingLotSystem.park("Toyota Fortuner");
+        parkingLotSystem.park("Maruti Swift Dzire");
+        parkingLotSystem.park("Tata Hexa");
+        Assert.assertEquals(owner.getFlag(), ParkingLotOwner.Flag.PARKING_IS_VACANT);
+    }
+
+    @Test
+    public void givenParkingLotIsFull_SecurityStaffShouldBeUpdated() throws ParkingLotException {
+        parkingLotSystem.register(airportSecurity);
+        parkingLotSystem.park("Tata Indigo CS");
+        parkingLotSystem.park("Toyota Fortuner");
+        parkingLotSystem.park("Maruti Swift Dzire");
+        parkingLotSystem.park("Tata Hexa");
+        parkingLotSystem.park("Maruti 800");
+        Assert.assertEquals(true, airportSecurity.isParkingFull());
+    }
+
+    @Test
+    public void givenParkingLotIsNotFull_SecurityStaffShouldBeUpdated() throws ParkingLotException {
+        parkingLotSystem.register(airportSecurity);
+        parkingLotSystem.park("Tata Indigo CS");
+        parkingLotSystem.park("Toyota Fortuner");
+        parkingLotSystem.park("Maruti Swift Dzire");
+        Assert.assertFalse(airportSecurity.isParkingFull());
     }
 }
