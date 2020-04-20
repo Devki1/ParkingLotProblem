d0e9a2a7f13f19ca84fcdb60d5e07bb596a9c1ed
Devki1 <gdev3195@gmail.com>
2020-04-20 12:51:28 +0530

    REFACTORED:Used hashmap instaed of arraylist

--- src/main/java/com/bridgelabz/parkinglot/Observer/ParkingLotOwner.java
+++ src/main/java/com/bridgelabz/parkinglot/Observer/ParkingLotOwner.java
@@ -1,6 +1,11 @@
 package com.bridgelabz.parkinglot.Observer;
 
+import com.bridgelabz.parkinglot.service.ParkingLotSystem;
+
+import java.util.HashMap;
+
 public class ParkingLotOwner implements ParkingLotObserver {
+
     public enum Flag {PARKING_IS_VACANT, PARKING_IS_FULL};
 
     Flag flag;
@@ -10,7 +15,15 @@ public class ParkingLotOwner implements ParkingLotObserver {
     }
 
     @Override
-    public void sendParkingStatus(int currentOccupiedSlots, int parkingLotCapacity) {
-        flag = (currentOccupiedSlots >= parkingLotCapacity) ? Flag.PARKING_IS_FULL : Flag.PARKING_IS_VACANT;
+    public void sendParkingStatus(HashMap<Integer, String> parkingLot) {
+        flag = (ParkingLotSystem.isParkingLotFull(parkingLot)) ? Flag.PARKING_IS_FULL : Flag.PARKING_IS_VACANT;
+    }
+
+    public Integer getEmptyParkingSlot(HashMap<Integer, String> parkingLot) {
+        for (int i = 1; i <= parkingLot.size(); i++) {
+            if (parkingLot.get(i) == null)
+                return i;
+        }
+        return null;
     }
 }
