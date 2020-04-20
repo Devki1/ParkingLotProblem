08ddfc57fd36bc1e2b06219f5686d4e344ce1c90
Devki1 <gdev3195@gmail.com>
2020-04-20 12:54:47 +0530

    REFACTORED:Deligate parking job of  parking Attendant

--- src/main/java/com/bridgelabz/parkinglot/service/ParkingLotSystem.java
+++ src/main/java/com/bridgelabz/parkinglot/service/ParkingLotSystem.java
@@ -1,23 +1,28 @@
 package com.bridgelabz.parkinglot.service;
 
+import com.bridgelabz.parkinglot.Observer.ParkingAttendant;
 import com.bridgelabz.parkinglot.Observer.ParkingLotObserver;
 import com.bridgelabz.parkinglot.Observer.ParkingLotException;
 import com.bridgelabz.parkinglot.Observer.Subject;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class ParkingLotSystem implements Subject {
 
-    private int parkingLotCapacity;
-    private String vehicleName;
-    private List<String> parkingLot;
+    public int parkingLotSize;
+    public String vehicleName;
+    public HashMap<Integer, String> parkingLot;
     private List<ParkingLotObserver> observers = new ArrayList<ParkingLotObserver>();
+    ParkingAttendant parkingAttendant = new ParkingAttendant(5);
 
-    public ParkingLotSystem(int parkingLotCapacity) {
-        this.parkingLotCapacity = parkingLotCapacity;
-        this.parkingLot = new ArrayList<String>();
+    public ParkingLotSystem(int parkingLotSize) {
+        this.parkingLotSize = parkingLotSize;
+        this.parkingLot = new HashMap<>();
+        for (int itr = 1; itr <= parkingLotSize; itr++) {
+            parkingLot.put(itr, null);
+        }
     }
+
 
     @Override
     public void register(ParkingLotObserver o) {
@@ -32,27 +37,54 @@ public class ParkingLotSystem implements Subject {
     @Override
     public void notifyObservers() {
         for (ParkingLotObserver observer : observers) {
-            observer.sendParkingStatus(parkingLot.size(), parkingLotCapacity);
+            observer.sendParkingStatus(parkingLot);
         }
     }
 
 
     public void park(String vehicle) throws ParkingLotException {
-        if (parkingLot.size() >= parkingLotCapacity)
-            throw new ParkingLotException(ParkingLotException.ExceptionType.PARKING_LOT_FULL, "PARKING LOT IS FULL");
         this.vehicleName = vehicle;
-        parkingLot.add(vehicleName);
+        parkingLot = parkingAttendant.park(vehicleName, parkingLot);
         this.notifyObservers();
     }
 
-    public void unPark(String vehicle) throws ParkingLotException {
-        if (!(parkingLot.contains(vehicle)))
+    public boolean unPark(String vehicle) throws ParkingLotException {
+        if (!isVehiclePresentInLot(vehicle))
             throw new ParkingLotException(ParkingLotException.ExceptionType.VEHICLE_ALREADY_UNPARKED_OR_WRONG_VEHICLE, "VEHICLE IS ALREADY UNPARKED");
-        parkingLot.remove(vehicle);
+        Iterator<String> parkingLotIterator = getParkingLotIterator(parkingLot);
+        this.vehicleName = vehicle;
+        while (parkingLotIterator.hasNext()) {
+            if (parkingLotIterator.next().equals(vehicle)) {
+                parkingLotIterator.remove();
+                return true;
+            }
+        }
         this.notifyObservers();
+        return true;
+    }
+
+    private boolean isVehiclePresentInLot(String vehicle) {
+        Iterator<String> parkingLotIterator = getParkingLotIterator(parkingLot);
+        while (parkingLotIterator.hasNext()) {
+            if (Objects.equals(parkingLotIterator.next(), vehicle))
+                return true;
+        }
+        return false;
+    }
+
+    private Iterator<String> getParkingLotIterator(HashMap<Integer, String> parkingLot) {
+        return parkingLot.values().iterator();
     }
 
     public boolean isVehicleParked() {
-        return parkingLot.contains(vehicleName);
+        return parkingLot.containsValue(vehicleName);
+    }
+
+    public static boolean isParkingLotFull(HashMap<Integer, String> parkingLot) {
+        for (int iteration = 1; iteration <= parkingLot.size(); iteration++) {
+            if (parkingLot.get(iteration) == null)
+                return false;
+        }
+        return true;
     }
 }
