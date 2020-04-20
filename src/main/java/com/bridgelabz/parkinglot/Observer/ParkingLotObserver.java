d478d5f923dcf93d72ff12c6ccb7545822795ccf
Devki1 <gdev3195@gmail.com>
2020-04-20 12:49:48 +0530

    REFACTORED:Used hashmap instead of arraylist

--- src/main/java/com/bridgelabz/parkinglot/Observer/ParkingLotObserver.java
+++ src/main/java/com/bridgelabz/parkinglot/Observer/ParkingLotObserver.java
@@ -1,6 +1,8 @@
 package com.bridgelabz.parkinglot.Observer;
 
+import java.util.HashMap;
+
 public interface ParkingLotObserver {
-    public void sendParkingStatus(int currentOccupiedSlots, int parkingLotCapacity);
+    public void sendParkingStatus(HashMap<Integer, String> parkingLot);
 }
 
