e5a87a750da6e1cc5aacbd460af73bb137e9c5f5
Devki1 <gdev3195@gmail.com>
2020-04-23 23:50:58 +0530

    REFACTORED:Code evenly distributes vehicle according to lots

--- src/main/java/com/bridgelabz/parkinglot/Observer/ParkingLotObserver.java
+++
@@ -1,8 +0,0 @@
-package com.bridgelabz.parkinglot.Observer;
-
-import java.util.HashMap;
-
-public interface ParkingLotObserver {
-    public void sendParkingStatus(HashMap<Integer, String> parkingLot);
-}
-
