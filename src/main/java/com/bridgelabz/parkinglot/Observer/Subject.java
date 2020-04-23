f0d05fd0d7187ef84f419ff6592221a179f17cc1
Devki1 <gdev3195@gmail.com>
2020-04-24 00:18:29 +0530

    REFACTORED:Code to evenly distributes vehicle along lots

--- src/main/java/com/bridgelabz/parkinglot/Observer/Subject.java
+++
@@ -1,7 +0,0 @@
-package com.bridgelabz.parkinglot.Observer;
-
-public interface Subject {
-    public void register(ParkingLotObserver o);
-    public void unRegister(ParkingLotObserver o);
-    public void notifyObservers();
-}
